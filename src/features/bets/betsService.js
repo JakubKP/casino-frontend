a710ca119a26e2bb4ce39e6ab7dbe7660a0e9bd4
Jakub <73079474+JakubKP@users.noreply.github.com>
2022-10-10 12:24:56 +0200

    Update betsService.js

--- src/features/bets/betsService.js
+++ src/features/bets/betsService.js
@@ -5,28 +5,28 @@ const API_URL = '/api/bets/'
 
 // Make bet
 const sendBet = async (betData) => {
-    const response = await axios.post(`http://localhost:5000${API_URL}bet`, betData)
+    const response = await axios.post(`https://for-fun-casino-api.onrender.com${API_URL}bet`, betData)
 
     return response.data
 }
 
 // Get coins
 const getCoins = async () => {
-    const response = await axios.get(`http://localhost:5000${API_URL}getcoins`)
+    const response = await axios.get(`https://for-fun-casino-api.onrender.com${API_URL}getcoins`)
 
     return response.data
 }
 
 // Get Bets
 const getBets = async () => {
-    const response = await axios.get(`http://localhost:5000${API_URL}getbets`)
+    const response = await axios.get(`https://for-fun-casino-api.onrender.com${API_URL}getbets`)
 
     return response.data
 }
 
 // Free coins
 const freeCoins = async () => {
-    const response = await axios.post(`http://localhost:5000${API_URL}freecoins`)
+    const response = await axios.post(`https://for-fun-casino-api.onrender.com${API_URL}freecoins`)
 
     return response.data
 }
